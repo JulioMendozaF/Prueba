3fc20b5b9c67538b38236283e622f51e6b6c22ed
Julio david Mendoza fuentes <juliodavidmendozafuentes@MacBook-Air-de-Julio.local>
2021-07-12 17:49:16 -0500

    Cambio por PDO en conexion a BD

--- conexion/db.php
+++ conexion/db.php
@@ -1,23 +1,17 @@
-
-<?php 
-
-$conexion=mysqli_connect("localhost","infinito_modulo","}~*4^zDUX8VQ");
-
-if (!$conexion){
-	
-	die ("Error de conexión ".mysqli_error($conexion));
-	
-	}
-	
- mysqli_select_db($conexion,"infinito_modulo") or die ("Error al conectar con la base de datos ".mysqli_error($conexion));
-
-
-
-function cerrarconexion(){
-	
-	mysqli_close($GLOBALS['conexion']);
-	
-	
-	}
-
-?>
+<?php
+  $servidor = "localhost";
+  $usuario = "infinito_modulo";
+  $password = "}~*4^zDUX8VQ";
+ 
+  try {
+        $conexion = new PDO("mysql:host=$servidor;dbname=nombreDeTuBaseDeDatos", $usuario, $password);      
+        $conexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        echo "Conexión realizada Satisfactoriamente";
+      }
+ 
+  catch(PDOException $e)
+      {
+      echo "La conexión ha fallado: " . $e->getMessage();
+      }
+ 
+  $conexion = null;
